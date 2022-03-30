827ba809c05a9b9788d71bb7cebd0b46558fc42e
Mert Kulac <96883175+MertKulac@users.noreply.github.com>
2022-03-30 21:03:49 +0300

    Create library.py

---
+++ library.py
@@ -0,0 +1,6 @@
+import paramiko  ###SSH LIBRARY###
+from paramiko_expect import \
+    SSHClientInteraction  ###TAKES AN ACTION ACCORDING TO INPUT CODE AND PROVIDES STOP THE CODE (expect)###
+from queue import Queue
+import time
+import getpass
